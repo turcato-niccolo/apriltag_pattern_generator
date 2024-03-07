2cf9502649fc337af1fed8292246ef426fc72901
turcato-niccolo <niccolo.turcato@phd.unipd.it>
2024-03-07 11:28:48 +0100

    tested on python 3.

---
+++ README.md
@@ -0,0 +1,20 @@
+# pattern_generator
+PDF pattern generator of AprilTags and AR tags
+## Dependencies
+* [*PyFPDF*](https://pyfpdf.readthedocs.io/en/latest/index.html)  a library for PDF document generation under Python
+* [*PyYAML*](https://pyyaml.org/)  a full-featured YAML framework for the Python
+```
+pip install fpdf pyyaml
+```
+## Usage
+* Configuration file [**cfg.yaml**](https://github.com/cgdsss/pattern_generator/blob/master/cfg.yaml)
+* Run scripts
+  ```
+  python tag.py
+  ```
+* A pdf file was generated, like [pattern.pdf](https://github.com/cgdsss/pattern_generator/blob/master/pattern.pdf)
+## Resources
+Here are some related links:
+* [AprilTag's home](https://april.eecs.umich.edu/software/apriltag/) 
+* [ROS AR_tag Detection](http://wiki.ros.org/ar_track_alvar)
+* [ROS AprilTag Detection](http://wiki.ros.org/apriltags2_ros)
---
+++ tag.py
@@ -0,0 +1,31 @@
+from fpdf import FPDF
+import yaml
+from yaml import Loader, Dumper
+import os
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+stream = open(os.path.join(current_path, 'cfg.yaml'), 'r')
+cfg = yaml.load(stream, Loader=Loader)
+
+tag_type = cfg['tag_type']
+tags_pose = cfg['tags_pose']
+tags_id = cfg['tags_id']
+tags_size = cfg['tags_size']
+pdf_size = cfg['pdf_size']
+
+pdf = FPDF(orientation = 'P', unit = 'cm', format=(pdf_size[0]*100.0, pdf_size[1]*100.0))
+pdf.add_page()
+print("Drawing: ")
+for i in range(len(tags_id)):
+    print("--")
+    print("id: ", str(tags_id[i]))
+    print("size: {0}m".format(tags_size[i]))
+    print("position: {0}m, {1}m".format(tags_pose[i][0], tags_pose[i][1]))
+    img = os.path.join(current_path, 'png/' + tag_type + "/" + str(tags_id[i]) + ".png")
+    pdf.image(img, ((tags_pose[i][0] - tags_size[i] / 2.0) + pdf_size[0]/2.0) * 100.0,
+        ((-tags_pose[i][1] - tags_size[i] / 2.0) + pdf_size[1]/2.0) * 100.0,
+        tags_size[i] * 100.0, tags_size[i] * 100.0)
+pdf_file = os.path.join(current_path, 'pattern.pdf')
+pdf.output(pdf_file, 'F')
+print("Done.")
+print("Already generated:", pdf_file)
